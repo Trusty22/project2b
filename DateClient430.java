a2892c14a5e349a27ba7459e49c84a4d20464a08
Mandeep Masoun <mandeepmasoun@yahoo.com>
2024-10-24 20:39:12 -0700

    stuff

--- DateClient430.java
+++ DateClient430.java
@@ -8,15 +8,17 @@ public class DateClient430
 {
     public static void main(String[] args)
     { 
-        /**   if (args.length != 1) {
-            System.err.println("Usage: java DateClient430 <port number>");
-            System.exit(1);
-        }
-
-        int port = Integer.parseInt(args[0]); */
+        
         try
         {
-            Socket sock = new Socket("127.0.0.1",1617);
+            if (args.length != 1) {
+                System.err.println("Usage: java DateClient430 <port number>");
+                 System.exit(1);
+            }
+
+            int port = Integer.parseInt(args[0]); 
+
+            Socket sock = new Socket("127.0.0.1",port);
             InputStream in = sock.getInputStream();
             BufferedReader bin = new BufferedReader(new InputStreamReader(in));
             String line;
