61dbf82539a71e2295a433f4c328e1647e75275b
Mandeep Masoun <mandeepmasoun@yahoo.com>
2024-10-24 20:33:55 -0700

    runin

--- DateClient430.java
+++ DateClient430.java
@@ -7,10 +7,16 @@ import java.io.*;
 public class DateClient430
 {
     public static void main(String[] args)
-    {
+    { 
+        /**   if (args.length != 1) {
+            System.err.println("Usage: java DateClient430 <port number>");
+            System.exit(1);
+        }
+
+        int port = Integer.parseInt(args[0]); */
         try
         {
-            Socket sock = new Socket("127.0.0.1",6013 );
+            Socket sock = new Socket("127.0.0.1",1617);
             InputStream in = sock.getInputStream();
             BufferedReader bin = new BufferedReader(new InputStreamReader(in));
             String line;
