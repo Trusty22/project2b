da561e8b587d511f394aaaf7f0bbe29749f84ff1
Mandeep Masoun <mandeepmasoun@yahoo.com>
2024-10-24 18:22:36 -0700

    format

--- DateServer.java
+++ DateServer.java
@@ -2,33 +2,35 @@ import java.net.*;
 import java.io.*;
 public class DateServer
 {
-public static void main(String[] args)
-{
-try
-{
-ServerSocket sock = new ServerSocket(6013);
-/* now listen for connections */
-while (true)
-{
-Socket client = sock.accept();
-PrintWriter pout = new PrintWriter(client.getOutputStream(),
-true);
-/* simulated 5000ms work*/
-try
-{
-Thread.sleep(5000);
-}
-catch (InterruptedException e) {}
-/* write the Date to the socket */
-pout.println(new java.util.Date().toString());
-/* close the socket and resume */
-/* listening for connections */
-client.close();
-}
-}
-catch (IOException ioe)
-{
-System.err.println(ioe);
-}
-}
+    public static void main(String[] args)
+    {
+        try
+        {
+            ServerSocket sock = new ServerSocket(6013);
+            /* now listen for connections */
+            while (true)
+            {
+                Socket client = sock.accept();
+                PrintWriter pout = new PrintWriter(client.getOutputStream(),
+                true);
+                /* simulated 5000ms work*/
+            try
+            {
+                Thread.sleep(5000);
+            }
+            catch (InterruptedException e) {}
+
+            /* write the Date to the socket */
+            pout.println(new java.util.Date().toString());
+
+            /* close the socket and resume */
+            /* listening for connections */
+            client.close();
+            }
+        }
+        catch (IOException ioe)
+        {
+            System.err.println(ioe);
+        }
+    }
 }
